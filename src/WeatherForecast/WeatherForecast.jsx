449ac0b451973104987c861069b8a546e731dd95
techbyadi <pssharma.aditya@gmail.com>
2024-08-21 17:02:41 -0700

    create a new component named WeatherForecast

---
+++ src/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import './WeatherForecast.css'
+
+
+const WeatherForecast = () => {
+  return (
+    <div className="weather">
+      <h2>Day of the Week</h2>
+      <img src="" alt="" />
+      <p><span>conditions: </span>current weather conditions</p>
+      <p><span>time: </span>time of day</p>
+    </div>
+  )
+}
+
+
+export default WeatherForecast;
